36346e4c915b2b8005431f1883108194a5e79142
Lucas Lazarini <lucalazarini@hotmail.com>
2019-08-01 19:22:23 -0300

    Register final

--- server/controllers/v1/games/retrieveGames.js
+++ server/controllers/v1/games/retrieveGames.js
@@ -1,9 +1,33 @@
 /**
- * @api {GET} /games/ List games
+ * @api {GET} /v1/games/ List games
  * @apiName List games
  * @apiGroup Games
  * @apiVersion 1.0.0
- */
+ *
+ * @apiSuccess (201) {String} data Games data.
+ * @apiSuccessExample {json} Success-Response:
+    {
+      "data": {
+        "_id": "1",
+        "name": "super milk bros",
+        "platform": "Switch",
+        "value": 50,25
+        "__v": 0
+      }
+    }
+ * @apiError (400) {String} data Error message.
+ * @apiError (404) {String} data Error message.
+ *
+ * @apiErrorExample {json} 400:
+    {
+      "data": "\"password\" is not allowed to be empty"
+    }
+ * @apiErrorExample {json} 409:
+    {
+      "data": "Data sent is violating a unique constraint."
+    }
+*/
+
 const constants = require('../../../utils/constants');
 const findDatabase = require('../../../utils/findDatabase');
 
