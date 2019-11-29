b5acfaa135a2b6e6210a3be179d91dc177f0e436
Lucas Lazarini <lucalazarini@hotmail.com>
2019-11-28 23:13:46 -0300

    Add some error constants

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -30,6 +30,9 @@ module.exports = {
     name: {
       DOCUMENT_NOT_FOUND_ERROR: 'DocumentNotFoundError',
       VALIDATION_ERROR: 'ValidationError',
+      NOT_FOUND: 'NotFoundError',
+      INVALID_AUTH: 'InvalidAuthError',
+      INVALID_SESSION: 'InvalidSessionError',
     },
     code: {
       UNIQUE_CONSTRAINT: 11000,
