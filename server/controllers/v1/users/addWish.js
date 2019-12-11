8a103b9c13d61c03715b529f9c418fabe48c2426
Lucas Lazarini <lucalazarini@hotmail.com>
2019-12-11 20:07:54 -0300

    Create add wish to user wishlist API

--- server/controllers/v1/users/addWish.js
+++ server/controllers/v1/users/addWish.js
@@ -1,15 +1,33 @@
 const updateDatabase = require('../../../utils/updateDatabase');
+const findDatabase = require('../../../utils/findDatabase');
 const constants = require('../../../utils/constants');
+const { InvalidPlatformId } = require('../../../utils/errors');
 
 module.exports = async (req, res, next) => {
   const { id, platformId } = req.body;
   const { user } = req;
-  const game = user.wishlist.find(wish => wish.id === id);
-  if (game) {
+
+  let games;
+
+  try {
+    games = await findDatabase(constants.tables.GAMES, { _id: id }, 0, 1);
+  } catch (err) {
+    return next(err);
+  }
+  const validPlatform = games.platforms.find(
+    platform => platform.gameId === platformId
+  );
+  if (!validPlatform) {
+    return next(new InvalidPlatformId());
+  }
+  let game = user.wishlist.find(wish => wish.id === id);
+  if (!game) {
+    user.wishlist.push({ id });
+    game = user.wishlist[user.wishlist.length - 1];
+  }
+  const platform = game.platformIds.find(platId => platId === platformId);
+  if (!platform) {
     game.platformIds.push(platformId);
-  } else {
-    console.log(0);
-    user.wishlist.push({ id, platformId });
   }
   let newUser;
   try {
