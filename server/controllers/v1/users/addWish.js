509b2343fcc9cd96a2a461851aa78ed80bb4b660
Guilherme Conti <guibasconti@gmail.com>
2020-01-15 02:33:53 -0300

    Get user info with wishlist on add and delete wish

--- server/controllers/v1/users/addWish.js
+++ server/controllers/v1/users/addWish.js
@@ -5,10 +5,20 @@ const { InvalidPlatformId } = require('../../../utils/errors');
 
 module.exports = async (req, res, next) => {
   const { id, platformIds } = req.body;
-  const { user } = req;
 
   let games;
-
+  let user;
+  try {
+    user = await findDatabase(
+      constants.tables.USERS,
+      { _id: req.user._id },
+      constants.selections.USER_WITH_WISHLIST,
+      0,
+      1
+    );
+  } catch (err) {
+    return next(err);
+  }
   try {
     games = await findDatabase(constants.tables.GAMES, { _id: id }, [], 0, 1);
   } catch (err) {
