48580d840943a57d9e72c8c73a678b3c59041b18
Guilherme Conti <guibasconti@gmail.com>
2020-02-04 23:14:39 -0300

    Add low res image to game model

--- server/models/Games.js
+++ server/models/Games.js
@@ -3,6 +3,7 @@ module.exports = mongoose => {
     name: { type: String, unique: true, required: true, },
     description: { type: String, required: false, },
     image: { type: String, required: false, default: '', },
+    lowResImage: { type: String, required: false, default: '', },
     categories: [ mongoose.Schema.Categories ],
     platforms: [ mongoose.Schema.Platforms ],
     enabled: { type: Boolean, required: false, default: true, },
